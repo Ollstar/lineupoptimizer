01442b164dfb36ef6ee488916cd3877698d2f287
Oliver Andrews <oltandrews@gmail.com>
2023-05-02 13:07:36 -0700

    pkg test

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Only copy files on the server-side build
-    if (isServer) {
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: 'node_modules/glpk.js/dist/glpk.wasm',
-              to: 'static/wasm/glpk.wasm',
-            },
-          ],
-        })
-      );
-    }
-
+  // other Next.js config options...
+  webpack: (config) => {
+    
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      glpk: path.resolve(__dirname, 'node_modules/glpk.js/dist/glpk.wasm'),
+    };
     return config;
   },
 };
