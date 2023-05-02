8290df98428c9dc9441ceaec0877a7c1c85be0b5
Oliver Andrews <oltandrews@gmail.com>
2023-05-02 07:40:17 -0700

    test config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack(config, options) {
+    // Add WebAssembly support
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
