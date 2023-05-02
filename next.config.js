30c7770bfccd17fc2220a63ef64b8c50ea998347
Oliver Andrews <oltandrews@gmail.com>
2023-05-02 12:38:30 -0700

    wasm in api folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['glpk.js']
-}
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = nextConfig;
+module.exports = {
+  // ...
+  webpack: (config, { isServer }) => {
+    // Add the copy plugin to copy glpk.wasm file to the api directory during build time
+    if (isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: './node_modules/glpk.js/dist/glpk.wasm',
+              to: './.next/server/pages/api/glpk.wasm',
+            },
+          ],
+        })
+      );
+    }
+    // ...
+    return config;
+  },
+};
